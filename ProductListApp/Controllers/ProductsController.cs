df0809ac42f4870a2dffd9ec7bdd9c5818f74e0e
Boban Milosevik <bobavtokom888@gmail.com>
2024-01-17 22:35:16 +0100

    Add project files.

---
+++ ProductListApp/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using ProductListApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProductListApp.Controllers
+{
+    public class ProductsController : ApiController {
+       readonly Product[] products = new Product[] {
+            new Product { Id = 1, Name = "Bobi", Category = "Programmer", Price = 85.4M},
+            new Product { Id = 2, Name = "Tivkiot Don", Category = "Books", Price = 15.5M},
+            new Product { Id = 1, Name = "Canyon", Category = "Bicycle", Price = 2424.8M}
+        };
+        IEnumerable<Product> GetProducts() {
+            return products;
+        }
+
+        IHttpActionResult GetProduct(int id) {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            if(product == null) {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
---
+++ ProductListApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProductListApp.WebApiApplication" Language="C#" %>
